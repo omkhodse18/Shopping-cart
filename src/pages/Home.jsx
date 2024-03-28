be9f8046cbdd48c9738885f21fb946d619c6ff4f
omkhodse18 <omkarkhodse.18@gmail.com>
2024-03-28 20:33:14 +0530

    Done with redux

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  const API_URL = "https://fakestoreapi.com/products";
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
+
---
+++ src/redux/Store.jsx
@@ -0,0 +1,9 @@
+import "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
+import { CartSlice } from "./Slices/CartSlice"
+
+export const store = configureStore({
+    reducer : {
+        cart : CartSlice
+    }
+}) 
